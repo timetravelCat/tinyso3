2121b89cb21c4c20a4957c75d2ecebc30960f271
timetravelCat <timetraveler930@gmail.com>
2024-06-12 15:17:32 +0900

    chore: Add support for default floating point precision in Kconfig

---
+++ include/tinyso3/Matrix.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file Matrix.hpp
+ * 
+ * A template based matrix library.
+ * 
+ * @author timetravelCat <timetraveler930@gmail.com>
+ */
+
+#pragma once
+
+#include "version.hpp"
+
--- include/tinyso3/version.hpp
+++ include/tinyso3/version.hpp
@@ -5,4 +5,14 @@
 #define TINYSO3_MAJOR_VERSION 0
 #define TINYSO3_MINOR_VERSION 0
 #define TINYSO3_PATCH_VERSION 0
-#define TINYSO3_TWEAK_VERSION "0f29a98"
+#define TINYSO3_TWEAK_VERSION "539fcff"
+
+#include "config.hpp"
+
+#define TYPE float
+
+#ifdef CONFIG_FLOATING_POINT_PRECISION_32
+#define TYPE float
+#elif CONFIG_FLOATING_POINT_PRECISION_64
+#define TYPE double
+#endif
