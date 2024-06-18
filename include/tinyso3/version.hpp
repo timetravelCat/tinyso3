743a47a174d9e943db91f8498d1add80e36062aa
timetravelCat <timetraveler930@gmail.com>
2024-06-19 08:20:44 +0900

    feat: Update version.hpp files to include default rotation matrix transformation convention

--- include/tinyso3/version.hpp
+++ include/tinyso3/version.hpp
@@ -5,7 +5,7 @@
 #define TINYSO3_MAJOR_VERSION 0
 #define TINYSO3_MINOR_VERSION 0
 #define TINYSO3_PATCH_VERSION 0
-#define TINYSO3_TWEAK_VERSION "38b5d08"
+#define TINYSO3_TWEAK_VERSION "4b582d8"
 
 #include "config.hpp"
 
