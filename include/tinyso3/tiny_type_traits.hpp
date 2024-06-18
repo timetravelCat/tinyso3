b735a46d3afee4385ee4fce89e7c3b7fe5406089
timetravelCat <timetraveler930@gmail.com>
2024-06-18 13:10:11 +0900

    feat: Add support for default rotation matrix transformation convention in Kconfig, Add integral_constant and is_same.

--- include/tinyso3/tiny_type_traits.hpp
+++ include/tinyso3/tiny_type_traits.hpp
@@ -26,4 +26,21 @@ struct enable_if<true, T> {
 template<bool B, typename T = void>
 using enable_if_t = typename enable_if<B, T>::type;
 
+template<class T, T v>
+struct integral_constant {
+    static constexpr T value = v;
+    using value_type = T;
+    using type = integral_constant; // using injected-class-name
+    constexpr operator value_type() const noexcept { return value; }
+    constexpr value_type operator()() const noexcept { return value; }
+};
+
+using true_type = integral_constant<bool, true>;
+using false_type = integral_constant<bool, false>;
+
+template<class T, class U>
+struct is_same : false_type {};
+
+template<class T>
+struct is_same<T, T> : true_type {};
 }; // namespace tinyso3
